111f0c0b355819a3cd4a0e2364148a04032f3935
Iain Learmonth <irl@sr2.uk>
2022-11-28 13:15:22 +0000

    fix(cert): Remove algorithm from tls_cert_request

--- certificate.tf
+++ certificate.tf
@@ -12,7 +12,6 @@ resource "tls_private_key" "cert_private_key" {
 }
 
 resource "tls_cert_request" "req" {
-  key_algorithm   = "RSA"
   private_key_pem = tls_private_key.cert_private_key.private_key_pem
   subject {
     common_name = "*.${trimsuffix(var.domain_name, ".")}"
@@ -20,16 +19,16 @@ resource "tls_cert_request" "req" {
 }
 
 resource "acme_certificate" "certificate" {
-  account_key_pem           = acme_registration.reg.account_key_pem
-  certificate_request_pem   = tls_cert_request.req.cert_request_pem
+  account_key_pem         = acme_registration.reg.account_key_pem
+  certificate_request_pem = tls_cert_request.req.cert_request_pem
 
   dns_challenge {
     provider = "rfc2136"
     config = {
-      RFC2136_NAMESERVER = var.rfc2136_nameserver
-      RFC2136_TSIG_KEY = var.rfc2136_tsig_key
-      RFC2136_TSIG_SECRET = var.rfc2136_tsig_secret
-      RFC2136_POLLING_INTERVAL = 60
+      RFC2136_NAMESERVER          = var.rfc2136_nameserver
+      RFC2136_TSIG_KEY            = var.rfc2136_tsig_key
+      RFC2136_TSIG_SECRET         = var.rfc2136_tsig_secret
+      RFC2136_POLLING_INTERVAL    = 60
       RFC2136_PROPAGATION_TIMEOUT = 360
     }
   }
