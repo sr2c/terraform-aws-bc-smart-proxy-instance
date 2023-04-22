4f5cee28bb35a9698fe8949db383ca7239366b93
Iain Learmonth <irl@sr2.uk>
2023-04-22 14:12:17 +0100

    feat: include group name in dns name to differentiate

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 locals {
   trimmed_dns_zone = trimsuffix(var.dns_zone, ".")
+  group_name = coalesce(module.this.tenant, "default")
 }
 
 module "conf_log" {
@@ -104,7 +105,7 @@ data "aws_route53_zone" "this" {
 
 resource "aws_route53_record" "this" {
   zone_id = data.aws_route53_zone.this.id
-  name    = "*.smart"
+  name    = "*.${local.group_name}.smart"
   type    = "A"
   ttl     = 180
 
@@ -154,7 +155,7 @@ resource "aws_iam_policy" "dns_validation" {
         Condition = {
           "ForAllValues:StringEquals" = {
             "route53:ChangeResourceRecordSetsNormalizedRecordNames" = [
-              "_acme-challenge.smart.${local.trimmed_dns_zone}"
+              "_acme-challenge.${local.group_name}.smart.${local.trimmed_dns_zone}"
             ]
             "route53:ChangeResourceRecordSetsRecordTypes" : [
               "TXT"
@@ -194,10 +195,10 @@ resource "tls_private_key" "cert_private_key" {
 
 resource "tls_cert_request" "req" {
   private_key_pem = tls_private_key.cert_private_key.private_key_pem
-  dns_names       = ["*.smart.${local.trimmed_dns_zone}"]
+  dns_names       = ["*.${local.group_name}.smart.${local.trimmed_dns_zone}"]
 
   subject {
-    common_name = "*.smart.${local.trimmed_dns_zone}"
+    common_name = "*.${local.group_name}.smart.${local.trimmed_dns_zone}"
   }
 }
 
